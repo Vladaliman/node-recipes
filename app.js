500056151efe9b91f9acf24d9349635d07ff986b
VladimirOstojic <vladaliman@gmail.com>
2020-09-18 11:53:12 +0200

    finished the new user signup and connected it to the frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
